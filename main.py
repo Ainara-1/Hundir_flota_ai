71d834b382ceb59b3ff364f18e22c27a479e927f
Ainara-1 <vilanuccii@yahoo.com>
2026-04-23 10:36:35 +0200

    primer commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from utils import crear_tablero
+
+tablero_jugador_disparos = crear_tablero()
+tablero_jugador_barcos = crear_tablero()
+print(tablero_jugador_disparos)
+print(tablero_jugador_barcos)
+
+tablero_rival_disparos = crear_tablero()
+tablero_rival_barcos = crear_tablero()
+print("-----------")
+print(tablero_rival_disparos)
+print(tablero_rival_barcos)
+
---
+++ utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def crear_tablero():
+    tablero = np.full((10,10)," ")
+    return tablero
