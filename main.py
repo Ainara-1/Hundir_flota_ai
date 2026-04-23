b56847c5d418cb0ec83c2a215ae8ac745e828c6c
Ainara-1 <vilanuccii@yahoo.com>
2026-04-23 16:12:42 +0200

    prueb-1

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 
-from utils import crear_tablero
+from utils import crear_tablero, colocar_barcos, disparar
+
+import time
 
 tablero_jugador_disparos = crear_tablero()
 tablero_jugador_barcos = crear_tablero()
-print(tablero_jugador_disparos)
-print(tablero_jugador_barcos)
+
 
 tablero_rival_disparos = crear_tablero()
 tablero_rival_barcos = crear_tablero()
-print("-----------")
-print(tablero_rival_disparos)
+
+#introducimos los barcos jugador
+li_bar_jug=[[(0,1),(0,2)],[(7,8),(8,8)]]
+#introducimos los barcos del rival
+li_bar_riv=[[(9,1),(9,2)],[(7,2),(7,1)]]
+
+tablero_jugador_barcos=colocar_barcos(li_bar_jug,tablero_jugador_barcos)
+tablero_rival_barcos=colocar_barcos(li_bar_riv,tablero_rival_barcos)
+
+print("      TABLERO RIVAL BARCOS      ")
 print(tablero_rival_barcos)
+print("          TABLERO JUGADOR BARCOS      ")
+print(tablero_jugador_barcos)
+time.sleep(3)
+print("Tu turno jugador")
+
+
+
 
